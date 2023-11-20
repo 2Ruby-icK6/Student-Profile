e86cdf01e828fbe5f8a545bcdff1fedf3c13447b
Delcoro, Jofli Asaph <asaphdelcoro987@gmail.com>
2023-11-20 09:03:13 +0800

    added crud functions in the province.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
         <h1>Menu</h1>
         <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="../index.php">Home</a></li>
             <li><a href="views/students.view.php">Students</a></li>
             <li><a href="views/towns.view.php">Town</a></li>
             <li><a href="#">Province</a></li>
